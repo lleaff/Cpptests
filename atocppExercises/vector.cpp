cdb9e2f32aa3fd218d95d32a484ba5311e0c4185
lleaff <lleaff@users.noreply.github.com>
2015-03-07 16:03:30 +0100

    base

---
+++ atocppExercises/vector.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+template<typename T> 
+void printVector(std::vector<T> vect)
+{
+	int vsize = vect.size();
+	for (int i = 0; i < vsize; ++i) {
+		std::cout << vect[i] << (i < vsize - 1 ? ", " : "");
+	}
+}
+
+int main()
+{
+	using namespace std;
+	vector<int> myVector = { 5, 9, -1, 200, 0 };
+	printVector(myVector);
+}
