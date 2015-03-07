1d0741ccf7d032bef80dc9f013c3a8c592264f67
lleaff <lleaff@users.noreply.github.com>
2015-03-07 16:09:57 +0100

    using standard header <algorithm>

--- atocppExercises/vector.cpp
+++ atocppExercises/vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 template<typename T> 
 void printVector(std::vector<T> vect)
@@ -15,4 +16,5 @@ int main()
 	using namespace std;
 	vector<int> myVector = { 5, 9, -1, 200, 0 };
 	printVector(myVector);
+	sort(myVector.begin(), myVector.end(), myVector);
 }
