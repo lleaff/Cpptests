51982e5c181c46e25f9bccf49aada21ddc25c47f
lleaff <lleaff@users.noreply.github.com>
2015-04-09 16:43:49 +0200

    > istreams are buffered

---
+++ acccpp/inputTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "input something..." << std::endl;
+	std::string something;
+	std::cin >> something;
+	std::cout << "You input: " << something << std::endl;
+	std::cout << "input something else..." << std::endl;
+	std::string smthelse;
+	std::cin >> smthelse;
+	std::cout << "You input: " << smthelse << std::endl;
+}
